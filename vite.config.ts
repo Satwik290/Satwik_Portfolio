ad7ae48e162686d07b8b5b1613d6b6c84e13eeae
satwik <mohanty.satwik290@gmail.com>
2026-02-01 21:53:31 +0530

    vite changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change this to '/your-repo-name/' when deploying to GitHub Pages
+  base: '/Satwik_Portfolio/', // Change this to '/your-repo-name/' when deploying to GitHub Pages
 })
